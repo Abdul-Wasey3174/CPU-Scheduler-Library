41398423cf3ac125356cf7f3ede5809a42e79d89
sav <s.valcourt@northeastern.edu>
2024-02-06 15:56:02 -0500

    Programming Assignment 2 Code

---
+++ PA2/libscheduler/libscheduler.c
@@ -0,0 +1,56 @@
+/*
+ * Programming Assignment 2
+ * CS 5600, Spring 2024
+ * Library for Scheduler Code
+ * February 6, 2024
+ */
+
+#include "libscheduler.h"
+
+/*
+ * You may need to define some global variables or a struct to 
+ * store your job queue elements. Feel free to do it here at the
+ * top of the .c file, or in the .h file.
+ */
+
+/* See Canvas function descriptions for details of each function */
+
+void scheduler_show_queue()
+{
+
+}
+
+int scheduler_new_job(int job_number, int time, int running_time, int priority)
+{
+	return -1;
+}
+
+int scheduler_job_finished(int job_number, int time)
+{
+	return -1;
+}
+
+int scheduler_quantum_expired(int time)
+{
+	return -1;
+}
+
+double scheduler_average_waiting_time()
+{
+	return 0;
+}
+
+double scheduler_average_turnaround_time()
+{
+	return 0;
+}
+
+double scheduler_average_response_time()
+{
+	return 0;
+}
+
+void scheduler_clean_up()
+{
+
+}
